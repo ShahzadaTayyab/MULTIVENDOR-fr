077a05590c1292898d6972dcc6115ab13b039f18
SHAHZADA TAYYAB <shahzadatayyabtanveer@gmail.com>
2025-07-30 19:40:30 +0500

    server url corrected

--- src/pages/UserInboxPage.jsx
+++ src/pages/UserInboxPage.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/styles";
-const ENDPOINT = "http://localhost:8080/";
+const ENDPOINT = "http://localhost:8000/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
