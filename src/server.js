b733f49d67dd6a1250136d58ea822a23d95f30a7
SHAHZADA TAYYAB <shahzadatayyabtanveer@gmail.com>
2025-07-30 12:37:13 +0500

    Project files added

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://876951e01903.ngrok-free.app";
+export const server = "https://7f42mlsd-8000.inc1.devtunnels.ms/";
 
-export const backend_url = "https://876951e01903.ngrok-free.app";
+export const backend_url = "https://7f42mlsd-8000.inc1.devtunnels.ms/";
