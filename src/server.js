3bd0edef5e974b6d0457f12a51aca6c93032c1f4
SHAHZADA TAYYAB <shahzadatayyabtanveer@gmail.com>
2025-07-30 12:45:22 +0500

    server url corrected

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://7f42mlsd-8000.inc1.devtunnels.ms/api/v2/";
+export const server = "https://7f42mlsd-8000.inc1.devtunnels.ms/api/v2";
 
 export const backend_url = "https://7f42mlsd-8000.inc1.devtunnels.ms/";
